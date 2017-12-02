87bf2b3080a7657703de9b45dae18d1a505ba766
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-02 20:00:34 +0100

    :tada: Generate Kitura project

---
+++ Sources/Application/Application.swift
@@ -0,0 +1,32 @@
+import Foundation
+import Kitura
+import LoggerAPI
+import Configuration
+import CloudEnvironment
+import KituraContracts
+import Health
+
+public let projectPath = ConfigurationManager.BasePath.project.path
+public let health = Health()
+
+public class App {
+    let router = Router()
+    let cloudEnv = CloudEnv()
+
+    public init() throws {
+    }
+
+    func postInit() throws {
+        // Capabilities
+        initializeMetrics(app: self)
+
+        // Endpoints
+        initializeHealthRoutes(app: self)
+    }
+
+    public func run() throws {
+        try postInit()
+        Kitura.addHTTPServer(onPort: cloudEnv.port, with: router)
+        Kitura.run()
+    }
+}
