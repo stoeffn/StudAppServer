d8d5927a1af315b2db3786519039f0a04bcd87d0
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-03 16:00:01 +0100

    :bug: Do not try to send multiple responses for one request

--- Sources/Application/Routes/StudApp/StudAppRoutes.swift
+++ Sources/Application/Routes/StudApp/StudAppRoutes.swift
@@ -1,20 +1,17 @@
 func initializeStudAppRoutes(in app: App) {
-    app.router.get("/studapp/privacy") { _, response, next in
-        defer { next() }
+    app.router.get("/studapp/privacy") { _, response, _ in
         try response
             .render("studapp/privacy", context: [:])
             .end()
     }
 
-    app.router.get("/studapp/help") { _, response, next in
-        defer { next() }
+    app.router.get("/studapp/help") { _, response, _ in
         try response
             .render("studapp/help", context: [:])
             .end()
     }
 
-    app.router.get("/studapp/?.*") { _, response, next in
-        defer { next() }
+    app.router.get("/studapp/?.*") { _, response, _ in
         try response
             .render("studapp/index", context: [:])
             .end()
