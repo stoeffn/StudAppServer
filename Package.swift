4f21d5b5a2f8ed0c724c40aef6136df4b0b2767c
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-03 16:07:34 +0100

    :heavy_plus_sign: Add OpenCloudKit

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Configuration.git", .upToNextMinor(from: "1.0.0")),
         .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
         .package(url: "https://github.com/IBM-Swift/Health.git", from: "0.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.0"))
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.0")),
+        .package(url: "https://github.com/BennyKJohnson/OpenCloudKit.git", .upToNextMinor(from: "0.5.0")),
     ],
     targets: [
         .target(name: "Stoeffn", dependencies: [.target(name: "Application"), "Kitura", "HeliumLogger"]),
         .target(name: "Application", dependencies: [
-            "Kitura", "Configuration", "CloudEnvironment", "SwiftMetrics", "Health", "KituraStencil"
+            "Kitura", "Configuration", "CloudEnvironment", "SwiftMetrics", "Health", "KituraStencil", "OpenCloudKit",
         ]),
 
         .testTarget(name: "ApplicationTests", dependencies: [.target(name: "Application"), "Kitura", "HeliumLogger"]),
