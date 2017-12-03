9369a4696f1068394f930b783d5527b81695ea2d
stoeffn <13511737+stoeffn@users.noreply.github.com>
2017-12-03 12:02:51 +0100

    :bug: Serve main StudApp page when no specific page was found

--- Sources/Application/Routes/StudApp/StudAppRoutes.swift
+++ Sources/Application/Routes/StudApp/StudAppRoutes.swift
@@ -1,15 +1,15 @@
 func initializeStudAppRoutes(in app: App) {
-    app.router.get("/studapp") { _, response, next in
-        defer { next() }
-        try response
-            .render("studapp/index", context: [:])
-            .end()
-    }
-
     app.router.get("/studapp/privacy") { _, response, next in
         defer { next() }
         try response
             .render("studapp/privacy", context: [:])
+            .end()
+    }
+
+    app.router.get("/studapp/?.*") { _, response, next in
+        defer { next() }
+        try response
+            .render("studapp/index", context: [:])
             .end()
     }
 }
