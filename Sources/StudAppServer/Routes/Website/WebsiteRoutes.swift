ac3a115b40f6bc980a180e55d73932ed3ffd5008
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-02-19 19:19:59 +0100

    :page_facing_up: Add automatically updating copyright message

--- Sources/StudAppServer/Routes/Website/WebsiteRoutes.swift
+++ Sources/StudAppServer/Routes/Website/WebsiteRoutes.swift
@@ -2,28 +2,36 @@ import Foundation
 import Kitura
 import OpenCloudKit
 
+var copyrightMessage: String {
+    let year = Calendar.current.component(.year, from: Date())
+    return "© Copyright Steffen Ryll, \(year)"
+}
+
 func initializeWebsiteRoutes(in router: Router) {
     router.get("/") { _, response, _ in
         try response
-            .render("studapp", context: [:])
+            .render("studapp", context: ["copyrightMessage": copyrightMessage])
             .end()
     }
 
     router.get("/legal/?") { _, response, _ in
         try response
-            .render("legal", context: [:])
+            .render("legal", context: ["copyrightMessage": copyrightMessage])
             .end()
     }
 
     router.get("/privacy/?") { _, response, _ in
         try response
-            .render("privacy", context: [:])
+            .render("privacy", context: ["copyrightMessage": copyrightMessage])
             .end()
     }
 
     router.get("/help/?") { _, response, _ in
         OrganizationRecord.fetch(desiredKeys: [.title, .iconThumbnail]) { result in
-            let context: [String: Any] = ["organizations": result.value ?? []]
+            let context: [String: Any] = [
+                "copyrightMessage": copyrightMessage,
+                "organizations": result.value ?? []
+            ]
             try? response
                 .render("help", context: context)
                 .end()
@@ -37,14 +45,14 @@ func initializeWebsiteRoutes(in router: Router) {
 
     router.get("/robots.txt") { _, response, _ in
         try response
-            .render("robots", context: [:])
+            .render("robots", context: ["copyrightMessage": copyrightMessage])
             .end()
     }
 
     router.get("/apple-app-site-association") { _, response, _ in
         response.headers.setType("json")
         try response
-            .render("apple-app-site-association", context: [:])
+            .render("apple-app-site-association", context: ["copyrightMessage": copyrightMessage])
             .end()
     }
 }
