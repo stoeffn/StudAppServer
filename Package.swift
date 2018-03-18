1ca7274ca765d193a8fac57b14654ca976cfc476
stoeffn <13511737+stoeffn@users.noreply.github.com>
2018-03-18 16:55:28 +0100

    :arrow_up: Upgrade dependencies

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 let package = Package(
     name: "StudAppServer",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.1.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.2.0")),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
-        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMinor(from: "6.0.0")),
+        .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", .upToNextMinor(from: "7.1.0")),
         .package(url: "https://github.com/IBM-Swift/Configuration.git", .upToNextMinor(from: "3.0.0")),
-        .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.4")),
-        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
-        .package(url: "https://github.com/olutsenko/OpenCloudKit.git", .upToNextMinor(from: "0.5.3")),
+        .package(url: "https://github.com/IBM-Swift/Health.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.5")),
+        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", .upToNextMinor(from: "2.2.1")),
+        .package(url: "https://github.com/BennyKJohnson/OpenCloudKit.git", .revision("c4a12722b9d19e669975cb12354521dc1c74cd37")),
     ],
     targets: [
         .target(name: "StudAppServer", dependencies: [
